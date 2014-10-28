453624cbc05ecc66583262922f02a643d1f770b8
nikita-matrosov <nikita.matrosov@teamdev.com>
2014-10-28 09:13:02 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+/*
+var React = require('react');
+
+var TodoApp = require('./components/TodoApp.react');
+
+React.renderComponent(
+    <TodoApp />,
+    document.getElementById('todoapp')
+);*/
